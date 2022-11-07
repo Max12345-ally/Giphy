111b4475f8d0ceaa72f548cf30993a869d216ca0
maxbond <ceo@rf.ru>
2022-11-07 14:56:07 +0300

    25 images step. but not rendered

--- src/App.js
+++ src/App.js
@@ -2,53 +2,38 @@ import React from 'react';
 import './App.css';
 import SearchForm from './components/SearchForm';
 import SearchResults from './components/SearchResults.js';
-
-const images = [
-  {
-    id: 1,
-    url: 'https://media.giphy.com/media/3DsNP07nApt1eEyjvM/giphy.gif'
-  },
-  {
-    id: 2,
-    url: 'https://media1.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif'
-  },
-  {
-    id: 3,
-    url:
-      'https://www.thisiscolossal.com/wp-content/uploads/2018/04/agif2opt.gif'
-  },
-  {
-    id: 4,
-    url: 'https://media.giphy.com/media/3oEdva9BUHPIs2SkGk/giphy.gif'
-  },
-  {
-    id: 5,
-    url: 'https://media.giphy.com/media/3o6ozoD1ByqYv7ARIk/giphy.gif'
-  },
-  {
-    id: 6,
-    url: 'https://buffer.com/library/wp-content/uploads/2016/06/giphy.gif'
-  },
-  {
-    id: 7,
-    url: 'https://media4.giphy.com/avatars/default5.gif'
-  },
-  {
-    id: 8,
-    url: 'https://media2.giphy.com/media/3oEduPff5ErjNmlbwY/source.gif'
-  }
-];
-
+import { useState, useEffect } from 'react';
 
 
 function App() {
-  return (
-    <div>
-        <h1>Giphy</h1>
-        <SearchForm />
-        <SearchResults images={images} />
-    </div>
-  );
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const [images, setImages] = useState([]);
+  
+  const searchOptions = {
+    key: process.env.REACT_APP_GIPHY_KEY,
+    limit: 25,
+    offset: 0,
+    rating: 'G',
+    api: 'https://api.giphy.com/v1/gifs',
+    endpoint: '/search'
+  };
+
+  function getImages() {
+    const searchString = 'minions';
+    /* Build a URL from the searchOptions object */
+    const url = `${searchOptions.api}${searchOptions.endpoint}?api_key=${searchOptions.key}&q=${searchString} &limit=${searchOptions.limit}&offset=${searchOptions.offset}&rating=${searchOptions.rating}&lang=en`;
+    console.log(url)
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        setImages(response.data);
+      })
+      .catch(console.error);
+  }
+  
 }
 
 export default App;
